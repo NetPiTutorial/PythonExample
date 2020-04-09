2311baee4a58e4c81fe6b20f46bc0796415f8392
NetPiTutorial <63409136+NetPiTutorial@users.noreply.github.com>
2020-04-09 21:13:25 +0200

    change to "Hello world"

--- PythonSkript.py
+++ PythonSkript.py
@@ -1,28 +1,29 @@
 
-import serial
+#import serial
 
-ser = serial.Serial ( port = '/dev/ttyACM0',         \
-                      baudrate =  9600,              \
-                      bytesize = serial.EIGHTBITS,   \
-                      parity = serial.PARITY_NONE,   \
-                      stopbits = serial.STOPBITS_ONE )
+#ser = serial.Serial ( port = '/dev/ttyACM0',         \
+#                      baudrate =  9600,              \
+#                      bytesize = serial.EIGHTBITS,   \
+#                      parity = serial.PARITY_NONE,   \
+#                      stopbits = serial.STOPBITS_ONE )
 
-Out = raw_input("Hier Nachricht eingeben: ")
-Out = Out + "\n"
-Out = bytes(Out)
-ser.write(Out)
+#Out = raw_input("Hier Nachricht eingeben: ")
+#Out = Out + "\n"
+#Out = bytes(Out)
+#ser.write(Out)
 
-In = ""
+#In = ""
+#while True:
+#    if ser.in_waiting:
+#        symbol = ser.read(1)
+
+#        if symbol != "\n":
+#            In += symbol
+#        else:
+#            print(In)
+#            break
+
 while True:
-    if ser.in_waiting:
-        symbol = ser.read(1)
-
-        if symbol != "\n":
-            In += symbol
-        else:
-            print(In)
-            break
-
-
+  print("Hello World")
 
 
